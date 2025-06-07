32d10acca9895faf5d4544d1d269b0bb6ceb5ed7
fahri <fahrumfahriansyah1@gmail.com>
2025-06-07 18:30:08 +0700

    update

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -56,7 +56,8 @@ class ContactResource extends Resource
                 //
             ])
             ->actions([
-                // Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
